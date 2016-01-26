a3576f9f9711a53e707040d648106560df18e9b3
Max Buras <max.buras@student.uni-luebeck.de>
2016-01-27 00:06:42 +0100

    Added Problem 9

---
+++ problems/0009/0009.js
@@ -0,0 +1,17 @@
+var BaseProblem = require("../../BaseProblem");
+var problem = new BaseProblem(9);
+
+var s = 1000;
+
+// Code from the official solution
+problem.addSolution(function() {
+    for (var a = 3; a<= (s-3)/3; a++) {
+        for (var b = (a+1); b <= (s-1-a)/2; b++) {
+            var c = s-a-b;
+            if (c*c == a*a + b*b) {
+                return a*b*c;
+            }
+        }
+    }
+});
+module.exports = problem;
