e5383c3e8b38af296cdb432c4082648ded94eae6
Max Buras <max.thomae@googlemail.com>
2016-01-30 12:50:16 +0100

    Added Problem 12

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var path = require("path");
 
-var problemNumber = 11;
+var problemNumber = 12;
 var problemString = ("0000" + problemNumber).substr(-4,4);
 
 var problemPath = "./problems/" + problemString + "/" + problemString;
 var problem = require(problemPath);
 console.log(problem.getId())
 
-var iterations = 10;
+var iterations = 1;
 var solutions = problem.getSolutions();
 
 
---
+++ problems/0012/0012.js
@@ -0,0 +1,31 @@
+var BaseProblem = require("../../BaseProblem");
+var problem = new BaseProblem(12);
+
+var divisorLimit = 500;
+
+problem.addSolution(function() {
+    // Brute force solution
+    
+    var i = 1;
+    while (true) {
+        var number = getNthTriangleNumber(i);
+        //console.log(number);
+        var divisorCount = 0;
+        for (var j=0; j<Math.sqrt(number); j++) {
+            if (number % j == 0) {
+                divisorCount = divisorCount + 2;
+            }
+            
+            if (divisorCount >= divisorLimit) {
+                return number;
+            }
+        }
+        i++;
+    }
+});
+
+var getNthTriangleNumber = function(n) {
+    return (n * (n + 1)) / 2;
+}
+
+module.exports = problem;
