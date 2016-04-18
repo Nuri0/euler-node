02e54f6ca197db6bde07a0d5d336bee288f6915a
Max Buras <max.thomae@googlemail.com>
2016-04-18 14:10:44 +0200

    Added Problem 21

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var path = require("path");
 
-var problemNumber = 20;
+var problemNumber = 21;
 var problemString = ("0000" + problemNumber).substr(-4,4);
 
 var problemPath = "./problems/" + problemString + "/" + problemString;
---
+++ problems/0021/0021.js
@@ -0,0 +1,35 @@
+var BaseProblem = require("../../BaseProblem");
+var problem = new BaseProblem(21);
+
+problem.addSolution(function() {
+	var numbers = [];
+	for (var i=1; i<10000; i++) {
+		numbers[i] = sumOfProperDivisors(i);
+	}
+	
+	var sum = 0;
+	for (var i=0; i<10000; i++) {
+		var sopd = numbers[i];
+		if (numbers[sopd] == i && i != sopd) {
+			sum += i;
+			console.log(i + " - " + sopd);
+		}
+	}
+	
+	return sum;
+});
+
+var sumOfProperDivisors = function(number) {
+	var sum = 1; // Skip the "1" in the for-loop, as we don't want to add number/1
+	for (var i=2; i<=Math.sqrt(number); i++) {
+		if (number % i == 0) {
+			sum += i;
+			if (number != i*i) {
+				sum += (number/i);
+			}
+		}
+	}
+	return sum;
+}
+
+module.exports = problem;
