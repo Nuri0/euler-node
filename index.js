96642231a1bed2a7bf0043a7b0dc0aa6800d8df6
Max Buras <max@8uras.org>
2016-05-20 23:52:16 +0200

    Added Problem 29

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var path = require("path");
 
-var problemNumber = 28;
+var problemNumber = 29;
 var problemString = ("0000" + problemNumber).substr(-4,4);
 
 var problemPath = "./problems/" + problemString + "/" + problemString;
---
+++ problems/0029/0029.js
@@ -0,0 +1,23 @@
+var BaseProblem = require("../../BaseProblem");
+var problem = new BaseProblem(29);
+var big = require("biginteger");
+
+problem.addSolution(function() {
+    var powers = [];
+    var count = 0;
+    
+    for (var a = 2; a <= 100; a++) {
+        for (var b = 2; b <= 100; b++) {
+            var value = big.BigInteger(a).pow(b);
+            if (!powers[value]) {
+                powers[value] = true;
+                count++;
+            }
+        }
+    }
+    
+    return count;
+});
+
+
+module.exports = problem;
