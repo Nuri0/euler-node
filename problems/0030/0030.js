0724f006bad870ee5ba4486793b933c5fafddc4c
Max Buras <max.thomae@googlemail.com>
2016-05-31 12:06:45 +0200

    Added Problem 30

---
+++ problems/0030/0030.js
@@ -0,0 +1,29 @@
+var BaseProblem = require("../../BaseProblem");
+var problem = new BaseProblem(30);
+
+problem.addSolution(function() {
+	// precompute the fifth digit powers
+	var powers = [];
+	for (var i=0; i<=9; i++) {
+		powers[i] = Math.pow(i,5);
+	}
+	
+	var sum = 0;
+	for (var i=10; i<500000; i++) {
+		var string = ""+i;
+		var powerSum = 0;
+		for (var j=0; j<string.length; j++) {
+			powerSum += powers[string[j]];
+		}
+		
+		if (powerSum == i) {
+			sum += i;
+		}
+		
+		
+	}
+	
+	return sum;
+	
+});
+module.exports = problem;
