b05ee8524a051f4b7c45ee910b06fecb7baf16de
Max Buras <max.thomae@googlemail.com>
2016-07-07 12:59:38 +0200

    Added Problem 34

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var path = require("path");
 
-var problemNumber = 33;
+var problemNumber = 34;
 var problemString = ("0000" + problemNumber).substr(-4,4);
 
 var problemPath = "./problems/" + problemString + "/" + problemString;
---
+++ problems/0034/0034.js
@@ -0,0 +1,33 @@
+var BaseProblem = require("../../BaseProblem");
+var problem = new BaseProblem(34);
+
+var factorials = [];
+
+problem.addSolution(function() {
+	
+	// precalculate factorials
+	factorials[0] = 1;
+	for (var i=1; i<10; i++) {
+		factorials[i] = factorials[i-1] * i;
+	}
+	
+	var sum = 0;
+	for (var i=11; i<1000000; i++) {
+		if (getDigitFactorialSum(i) == i)
+			sum += i;
+	}
+	
+	return sum;
+});
+
+var getDigitFactorialSum = function(number) {
+	var string = "" + number;
+	
+	var sum = 0;
+	for (var i=0; i<string.length; i++) {
+		sum += factorials[string[i]];
+	}
+	return sum;
+}
+
+module.exports = problem;
