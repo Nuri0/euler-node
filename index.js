9961b52263bdc0c428578b46e8970c37f9e0ae09
Max Buras <max.thomae@googlemail.com>
2016-07-12 15:26:56 +0200

    Added Problem 36

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var path = require("path");
 
-var problemNumber = 35;
+var problemNumber = 36;
 var problemString = ("0000" + problemNumber).substr(-4,4);
 
 var problemPath = "./problems/" + problemString + "/" + problemString;
---
+++ problems/0036/0036.js
@@ -0,0 +1,29 @@
+var BaseProblem = require("../../BaseProblem");
+var problem = new BaseProblem(36);
+
+problem.addSolution(function() {
+	
+	var sum = 0;
+	for (var i=0; i<1000000; i++) {
+		if (isPalindromic(""+i)) {
+			if (isPalindromic(i.toString(2))) {
+				sum += i;
+			}
+		}
+	}
+	
+	return sum;
+});
+
+var isPalindromic = function(string) {
+	var length = string.length;
+	for (var i=0; i<length/2; i++) {
+		if (string[i] != string[length-1-i]) {
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+module.exports = problem;
