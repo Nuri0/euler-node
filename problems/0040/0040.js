774b2c03a002853e0260655de1787556305d9f85
Max Buras <max.thomae@googlemail.com>
2016-07-28 14:35:10 +0200

    Added Problem 40

---
+++ problems/0040/0040.js
@@ -0,0 +1,32 @@
+var BaseProblem = require("../../BaseProblem");
+var problem = new BaseProblem(40);
+
+var positions = [1,10,100,1000,10000,100000,1000000];
+
+problem.addSolution(function() {
+	var result = 1;
+	var cnLength = 0; // == champernowneLength
+	var number = 1;
+	var positionCounter = 0;
+	
+	while(positionCounter < positions.length) {
+		var numberString = "" + number;
+		var numberLength = numberString.length;
+		
+		// is the wanted position inside the next number?
+		if (cnLength + numberLength >= positions[positionCounter]) {
+			var rest = positions[positionCounter] - cnLength;
+			// multiply the position to the result
+			result *= Number(numberString[rest-1]);
+			positionCounter++;
+		}
+		
+		// add the length of the current number
+		cnLength += numberLength;
+		number++;
+	}
+	
+	return result;
+});
+
+module.exports = problem;
